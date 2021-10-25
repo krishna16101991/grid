3ae3a6094c0a803f581f61ead1bc77149fea38dd
krishna16101991 <91254223+krishna16101991@users.noreply.github.com>
2021-10-25 15:17:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.post("/sendemail", (req, res) => {
   res.redirect("/sent");
 });
 
-const PORT = process.env.PORT || 587;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
